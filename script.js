6087cfb81e5d22d4c4e49056eb3bd334ac2311b1
EirianAlin <111126968+EirianAlin@users.noreply.github.com>
2023-02-25 15:15:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////
+// Set current year:
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
